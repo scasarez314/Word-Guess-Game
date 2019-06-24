cf2e49baf2b85ff6b0fe408892972101cd8fd01c
Sierra Casarez <scasarez314@gmail.com>
2019-06-24 12:31:52 -0500

    added somemore psuedosoding notes to the game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,2 +1,8 @@
 // get the cumputer to randomly choose a word
+//  ^^math to randomly choose.
 // have the user guess the word 
+// when they guess the letters they have chosen should show up on screen
+// ^^^probably going to need the onkeydown
+// when they guess wrong the counter should go down by increments of one."--"
+//track wins should go up by increments of one "++"
+// 
